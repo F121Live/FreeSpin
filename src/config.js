f2b8cd0e4446138ef05dfd6eab1393be6ad5e3a1
F121Live <got_facu@hotmail.com>
2022-07-10 10:21:28 -0300

    [Base] Add configs, add routes

---
+++ src/config.js
@@ -0,0 +1,30 @@
+var config = {};
+
+// Server
+config.PORT = 9001; // Default port
+config.DB_HOST = 'localhost'; // Default host
+config.DB_NAME = 'freespin'; // Default database name
+config.DB_USER = 'root'; // Default user
+config.DB_PASSWORD = 'CHANGE_ME'; // Default password
+config.DB_PORT = 3306; // Default port
+config.INFO_FILE = './info_config.json'; // Default info file
+config.EVENT_FILE = './event_config.json'; // Default event file
+config.CAMPAIGN_FILE = './campaign_config.json'; // Default campaign file
+config.DEBUG_LOG = true; // Default, log activity to console
+// Game
+config.AVAILABLE_CHARACTERS = './character_config.json'  // Default characters, used for account creation and update
+config.AVAILABLE_CHAO = './chao_config.json' // Default chao, used for account creation and update
+config.USE_ENERGY = true; // Default, use the lives system
+config.MAX_ENERGY = 10; // Default, max lives
+config.ENERGY_REFILL_TIME = 5000; // Default, energy refill time, miliseconds
+config
+config.SUPPORT_LEGACY_VERSIONS = false; // Default, support legacy versions of Sonic Runners
+config.CURRENT_VERSION = "2.2.0" // Current client version, if the client is older than this, the connection will be rejected
+
+// DEVELOPMENT
+//config.LOVES_MIKU = true; // Default, user loves Hatsune Miku
+//config.HATES_GTA_ONLINE = true; // Default, user hates GTA Online
+//config.PLAYS_LEAGE_OF_LEGENDS = false; // Default, user plays League of Legends, crash the app as soon as possible
+
+// Export config
+module.exports = config;
--- src/start.js
+++ src/start.js
@@ -3,10 +3,11 @@ const bodyParser = require('body-parser');
 
 // Iinitialize
 const app = express();
+var config = require('./config');
 
 // Settings
-app.set('port', process.env.PORT || 9001);
-app.use(bodyParser.urlencoded({extended: false}));
+app.set('port', config.PORT || 9001);
+app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 
 // Endpoints
@@ -15,4 +16,6 @@ app.use(require('./endpoints/index'));
 // Listener
 app.listen(app.get('port'), () => {
     console.log('Server is in port', app.get('port'));
+    console.log('Reading config file...');
+    console.log(config);
   });
