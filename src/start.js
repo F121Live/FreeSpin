106554766d4a24790d761470151f59178d020824
F121Live <got_facu@hotmail.com>
2022-07-10 10:41:48 -0300

    [Base] Process Maintenance Mode before listening

--- src/start.js
+++ src/start.js
@@ -4,11 +4,17 @@ const bodyParser = require('body-parser');
 // Iinitialize
 const app = express();
 var config = require('./config');
+var maintenance = false;
 
 // Settings
 app.set('port', config.PORT || 9001);
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
+if (process.argv = "--maintenance") {
+  console.log('NOTICE: Server is running in maintenance mode!!!!');
+  maintenance = true;
+}
+module.exports = maintenance;
 
 // Endpoints
 app.use(require('./endpoints/index'));
@@ -16,9 +22,6 @@ app.use(require('./endpoints/index'));
 // Listener
 app.listen(app.get('port'), () => {
     console.log('Server is in port', app.get('port'));
-    if (process.argv = "maintenance") {
-        console.log('NOTICE: Server is running in maintenance mode!!!!');
-    }
     console.log('Reading config file...');
     console.log(config);
   });
